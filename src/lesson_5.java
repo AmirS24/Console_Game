f55b716341a70a1e0c05de5f5be060784a3836c4
AmirS24 <bakeevamir1@gmail.com>
2025-10-17 12:37:22 +0600

    Обновлён lesson_5.java

--- src/lesson_5.java
+++ src/lesson_5.java
@@ -1,4 +1,14 @@
 public class lesson_5 {
     Car myCar = new Car(2020, "BMW X6", 3.5);
 
+    public static class Car {
+        int year;
+        String model;
+        double engine;
+        public Car(int theYear, String theModel, double theEngine){
+            year = theYear;
+        model = theModel;
+        engine = theEngine;
+        }
+    }
 }
